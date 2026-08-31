1561362f1203bd807756b22c911688bd6deb19de
paceletics <martin.l.watkinson@gmail.com>
2026-08-31 21:33:10 +0100

    Refresh Power of 10 dashboard module

--- paceletics-session-engine.js
+++ paceletics-session-engine.js
@@ -3,6 +3,6 @@
 if(typeof document==='undefined'||typeof document.write!=='function')return;
 document.write('<script src="paceletics-session-engine-core.js?v=1.0.0"><\/script>');
 if(/Athlete Dashboard/i.test(document.title)){
-  document.write('<script src="paceletics-powerof10.js?v=1.0.0"><\/script>');
+  document.write('<script src="paceletics-powerof10.js?v=1.1.0"><\/script>');
 }
 })();
